1246a3a56fdb9d87c7262f1f1491b0dbd0a6cbf4
pangyuworld <a904237539@qq.com>
2021-04-22 10:45:27 +0800

    添加java8支持

---
+++ src/main/java/com/pang/Config.java
@@ -0,0 +1,14 @@
+package com.pang;
+
+/**
+ * @author Pang
+ * @date 2021/4/22
+ */
+public class Config {
+    /*申请的gitee token*/
+    public static final String ACCESS_TOKEN_VALUE = "";
+    /*gitee 用户名*/
+    public static final String GITEE_USER_NAME = "";
+    /*gitee 的仓库名*/
+    public static final String GITEE_RESP_NAME = "";
+}
--- src/main/java/com/pang/Main.java
+++ src/main/java/com/pang/Main.java
@@ -6,13 +6,14 @@ import okhttp3.*;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.LinkedList;
 import java.util.List;
+
+import static com.pang.Config.*;
 
 /**
  * @author Pang
@@ -22,12 +23,6 @@ public class Main {
     private static final OkHttpClient CLIENT = new OkHttpClient().newBuilder()
             .build();
     private static final String ACCESS_TOKEN = "access_token";
-    /*申请的gitee token*/
-    private static final String ACCESS_TOKEN_VALUE = "";
-    /*gitee 用户名*/
-    private static final String GITEE_USER_NAME = "";
-    /*gitee 的仓库名*/
-    private static final String GITEE_RESP_NAME = "";
     private static final String CONTENT = "content";
     private static final String MESSAGE = "message";
     private static final String MESSAGE_VALUE = "上传图片";
@@ -36,24 +31,29 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        List<String> result=new LinkedList<>();
+        List<String> result = new LinkedList<>();
 
         for (String path : args) {
             JSONObject param = new JSONObject();
             File file = new File(path);
             String fileName = file.getName();
-            InputStream fileStream = new FileInputStream(file);
-            byte[] fileBytes = fileStream.readAllBytes();
+            // 读取文件内容
+            byte[] fileBytes = readFileContent(file);
+            // 进行base64编码
             String encodedFileString = Base64.getEncoder().encodeToString(fileBytes);
+            // 文件远程路径的url编码
             String remoteFilePath = "img/" + System.currentTimeMillis() + "/" + fileName;
-            String realUploadUrl = GITEE_UPLOAD_URL + URLEncoder.encode(remoteFilePath, StandardCharsets.UTF_8);
+            // 实际上传url
+            String realUploadUrl = GITEE_UPLOAD_URL + URLEncoder.encode(remoteFilePath);
+            // 上传的文件类型
             MediaType mediaType = MediaType.parse("application/json;charset=UTF-8");
+            // 构建参数
             param.put(ACCESS_TOKEN, ACCESS_TOKEN_VALUE);
             param.put(CONTENT, encodedFileString);
             param.put(MESSAGE, MESSAGE_VALUE);
-
+            // 构建访问请求
             RequestBody body = RequestBody.create(mediaType, param.toJSONString());
-
+            // 访问
             Request request = new Request.Builder()
                     .url(realUploadUrl)
                     .method("POST", body)
@@ -64,13 +64,13 @@ public class Main {
                     .addHeader("Referer", "https://gitee.com/api/v5/swagger")
                     .addHeader("Accept-Language", "zh,en;q=0.9,zh-CN;q=0.8")
                     .build();
-
-
+            // 获取访问结果
             Response response = CLIENT.newCall(request).execute();
-
+            // 将结果转换为json
             JSONObject responseJsonObj = JSON.parseObject(response.body().string());
-
+            // 获取到下载链接
             String downloadPath = responseJsonObj.getJSONObject("content").getString("download_url");
+            // 添加到结果
             result.add(downloadPath);
         }
 
@@ -80,4 +80,22 @@ public class Main {
 
     }
 
+    /**
+     * 读取文件内容
+     *
+     * @param file 文件
+     * @return 文件内容的编码
+     */
+    public static byte[] readFileContent(File file) {
+        byte[] fileBytes = new byte[0];
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            fileBytes = new byte[(int) file.length()];
+            fileInputStream.read(fileBytes);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileBytes;
+    }
 }
